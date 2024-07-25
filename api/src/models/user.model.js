cb254f8f5cc50ad4b475975a6e62bc81ba429165
farrukhan11 <97164927+farrukhan11@users.noreply.github.com>
2024-07-26 02:37:50 +0500

    post and user models

--- api/src/models/user.model.js
+++ api/src/models/user.model.js
@@ -23,6 +23,12 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: null,
     },
+    posts: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Post",
+      },
+    ],
   },
   { timestamps: true }
 );
